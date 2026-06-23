83eed02246acaa8504fbe6cda40848c462900e32
Hazziely Bernache Fernández <hbernachef1100@alumnos.ipn.edu.mx>
2026-06-22 20:08:36 -0600

    Se simulan lo eventos ordenados correctamente

--- extension/content/modulos/reactHack.js
+++ extension/content/modulos/reactHack.js
@@ -20,6 +20,15 @@
         
         console.log('[PartyHazz-ReactHack] Propiedades de React disponibles en el slider:', Object.keys(props));
         
+        // Fase 0: Iniciar el arrastre (vital porque muchos sliders ignoran cambios si no están en estado "isDragging")
+        if (props.onMouseDown) {
+           console.log('[PartyHazz-ReactHack] Disparando onMouseDown');
+           props.onMouseDown(fakeEvent);
+        } else if (props.onPointerDown) {
+           console.log('[PartyHazz-ReactHack] Disparando onPointerDown');
+           props.onPointerDown(fakeEvent);
+        }
+        
         // Fase 1: Actualizar el valor visual (Scrub)
         if (props.onChange) { 
           props.onChange(fakeEvent); 
@@ -32,12 +41,15 @@
         }
         
         // Fase 2: Confirmar el salto (Commit)
-        const eventosCommit = ['onChangeCommitted', 'onMouseUp', 'onPointerUp', 'onTouchEnd', 'onSeek', 'onSlidingComplete', 'onDragEnd'];
-        for (let evtName of eventosCommit) {
-           if (props[evtName]) {
-              console.log('[PartyHazz-ReactHack] Disparando evento de confirmación:', evtName);
-              props[evtName](fakeEvent);
-           }
+        if (props.onMouseUp) {
+           console.log('[PartyHazz-ReactHack] Disparando onMouseUp');
+           props.onMouseUp(fakeEvent);
+        } else if (props.onPointerUp) {
+           console.log('[PartyHazz-ReactHack] Disparando onPointerUp');
+           props.onPointerUp(fakeEvent);
+        } else if (props.onChangeCommitted) {
+           console.log('[PartyHazz-ReactHack] Disparando onChangeCommitted');
+           props.onChangeCommitted(fakeEvent);
         }
       } else {
         console.warn('[PartyHazz-ReactHack] No se encontró __reactProps$ en el slider');
