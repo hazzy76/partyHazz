206001b5bf2dd27cef3fa73e6863db7db1268672
Hazziely Bernache Fernández <hbernachef1100@alumnos.ipn.edu.mx>
2026-06-23 20:39:42 -0600

    Corrección en funcionalidad

--- extension/content/modulos/controladorVideo.js
+++ extension/content/modulos/controladorVideo.js
@@ -16,6 +16,7 @@ window.PartyHazz = window.PartyHazz || {};
 window.PartyHazz.controladorVideo = (() => {
   let videoEl = null;
   let esAccionSync = false;       // true = ignorar el proximo evento del video
+  let timerSyncLock = null;
   let callbackEvento = null;      // funcion a llamar cuando el usuario interactua
   let esperandoParaReproducir = false;
   let tiempoDestino = null;
@@ -99,16 +100,23 @@ window.PartyHazz.controladorVideo = (() => {
         esperandoParaReproducir = false;
         setSyncLock();
         videoEl.play().catch(err => console.warn('[PartyHazz] Play falló en canplay:', err));
+        releaseSyncLock(1000);
       }
     });
   }
 
   // --------------------------------------------------------------------------
-  // Cambia esAccionSync a true por 3 seg. se manera asincrona para que ignore los eventos de pausa, seeked, etc que se generen
+  // Bloqueo de bucles infinitos
   // --------------------------------------------------------------------------
 
   function setSyncLock() {
     esAccionSync = true;
+    if (timerSyncLock) clearTimeout(timerSyncLock);
+  }
+
+  function releaseSyncLock(delay = 1000) {
+    if (timerSyncLock) clearTimeout(timerSyncLock);
+    timerSyncLock = setTimeout(() => { esAccionSync = false; }, delay);
   }
 
   // --------------------------------------------------------------------------
@@ -193,15 +201,13 @@ window.PartyHazz.controladorVideo = (() => {
     if (!videoEl) return;
     if (Math.abs(videoEl.currentTime - time) > 0.5) {
       tiempoDestino = time;
-      esAccionSync = true;
-
+      
       await saltoSeguro(time);
 
       // Una vez terminado el salto, damos 500ms de gracia para atrapar 
       // y descartar el evento 'seeked' nativo de Bitmovin.
       setTimeout(() => { 
         tiempoDestino = null; 
-        esAccionSync = false;
       }, 500);
     }
   }
@@ -214,9 +220,11 @@ window.PartyHazz.controladorVideo = (() => {
     if (videoEl.readyState < 3) {
       esperandoParaReproducir = true;
       console.log('[PartyHazz] Esperando buffering antes de dar Play...');
+      releaseSyncLock(1000);
     } else {
       esperandoParaReproducir = false;
       videoEl.play().catch(err => console.warn('[PartyHazz] Play falló:', err));
+      releaseSyncLock(1000);
     }
   }
 
@@ -226,12 +234,14 @@ window.PartyHazz.controladorVideo = (() => {
     esperandoParaReproducir = false;
     await moverTiempo(time);
     videoEl.pause();
+    releaseSyncLock(1000);
   }
 
   async function aplicarSeek(time) {
     if (!videoEl) return;
     setSyncLock();
     await moverTiempo(time);
+    releaseSyncLock(1000);
   }
 
   // --------------------------------------------------------------------------
